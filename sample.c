50fc65037036570dd1b23fc64dbead4ba27cab45
esarla01 <erinsarlak003@gmail.com>
2023-04-18 18:47:45 -0400

    "Bit_getu is extracted to the um_execute."

---
+++ sample.c
@@ -0,0 +1,31 @@
+static inline uint64_t shl(uint64_t word, unsigned bits)
+{
+        assert(bits <= 64);
+        if (bits == 64)
+                return 0;
+        else
+                return word << bits;
+}
+
+/*
+ * shift R logical
+ */
+static inline uint64_t shr(uint64_t word, unsigned bits)
+{
+        assert(bits <= 64);
+        if (bits == 64)
+                return 0;
+        else
+                return word >> bits;
+}
+
+_
+uint64t Bitpack_getu(uint64_t word, unsigned width, unsigned lsb)
+{
+        unsigned hi = lsb + width; /* one beyond the most significant bit */
+        assert(hi <= 64);
+        /* different type of right shift */
+        return shr(shl(word, 64 - hi),
+                   64 - width); 
+}
+
